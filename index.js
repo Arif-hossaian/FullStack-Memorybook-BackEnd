66691924494738449766a7be116dfbe3717cc108
Arif-hossaian <arifhossain42136@gmail.com>
2021-07-18 21:47:22 +0600

    Test of get & create post is succesfull

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import postRoutes from "./routes/posts.js"
 
 dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors());
 
 app.use("/posts", postRoutes)
